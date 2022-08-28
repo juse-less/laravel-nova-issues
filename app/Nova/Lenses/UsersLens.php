897440cf489249902b1528707f4a98e21f4329b7
Juse Less <me@juseless.dev>
2022-08-28 18:54:49 +0200

    Added UserLens

---
+++ app/Nova/Lenses/UsersLens.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Nova\Lenses;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\LensRequest;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Lenses\Lens;
+
+class UsersLens extends Lens
+{
+    /**
+     * Get the query builder / paginator for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\LensRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return mixed
+     */
+    public static function query(LensRequest $request, $query)
+    {
+        return $request->withOrdering($request->withFilters(
+            $query
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Name')
+                ->displayUsing(fn (): string => 'Unicorn'),
+
+            Text::make('Email'),
+
+            DateTime::make('Created At')
+                ->displayUsing(fn (string $createdAt): string => Carbon::make($createdAt)->toAtomString()),
+        ];
+    }
+
+    /**
+     * Get the cards available on the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return parent::actions($request);
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'users-lens';
+    }
+}
